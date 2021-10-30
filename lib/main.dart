dc2f6874262770f551ab04aa5f067e62eea53d04
Srujan Mupparapu <srujanmup@gmail.com>
2021-10-30 18:37:03 -0500

    Finished Account Creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,304 @@
+import 'package:drunk_proj/api_calls.dart' as api;
+import 'package:drunk_proj/constants.dart';
+import 'package:drunk_proj/data.dart';
+import 'package:drunk_proj/functions.dart';
+import 'package:drunk_proj/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
-import 'constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(AlcoWatch());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AlcoWatch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
+    return CupertinoApp(
+      builder: (context, child) => MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        child: child,
+      ),
+      home: Welcome(),
+      theme: CupertinoThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: background,
+        primaryColor: background,
+        textTheme: textTheme,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
+
+
+class Welcome extends StatelessWidget {
+
+  final name = new TextEditingController();
+  final number = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
       backgroundColor: background,
-      child: Text('AlcoWatch')
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              SizedBox(height: 140,),
+              CustomText(
+                text: "AlcoWatch",
+                color: white,
+                size: 50,
+              ),
+              CustomText(
+                text: "Know when you’re tipsy,\nwhile others monitor your safety.",
+                color: white,
+                size: 20,
+              ),
+              SizedBox(height: 50,),
+              CustomTextField(
+                controller: name,
+                placeholder: "Full Name",
+              ),
+              SizedBox(height: 20,),
+              CustomTextField(
+                controller: number,
+                placeholder: "Phone Number",
+              ),
+              SizedBox(height: 200,),
+              CustomButton(
+                text: "Get Started",
+                onPress: () {
+                  UserData.instance.name = name.text;
+                  UserData.instance.number = number.text;
+                  api.createUser();
+                  push(AddContacts(), context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+
+class AddContacts extends StatefulWidget {
+
+  @override
+  _AddContactsState createState() => _AddContactsState();
+}
+
+class _AddContactsState extends State<AddContacts> {
+
+  Contact primary;
+  List<Contact> secondaries = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: background,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+            SizedBox(height: 140,),
+            CustomText(
+              text: "AlcoWatch",
+              color: white,
+              size: 50,
+            ),
+            CustomText(
+              text: "Add your emergency contacts.",
+              color: white,
+              size: 20,
+            ),
+            SizedBox(height: 50,),
+            primaryContactSelector(),
+            SizedBox(height: 5,),
+            CustomText(
+              text: "Text and call this contact.",
+              size: 20,
+            ),
+            SizedBox(height: 25,),
+            secondaryContactsSelector(),
+            SizedBox(height: 5,),
+            CustomText(
+              text: "Only text these contacts.",
+              size: 20,
+            ),
+            Spacer(),
+            CustomButton(
+              text: "Continue",
+              onPress: () {
+                UserData.instance.secondaries.addAll(
+                  secondaries.map<ContactInfo>((c) => ContactInfo(c.displayName, c.phones[0].number))
+                );
+                UserData.instance.primary = ContactInfo(primary.displayName, primary.phones[0].number);
+                api.addContacts();
+
+              },
+            ),
+            SizedBox(height: 85,)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget primaryContactSelector() => Builder(
+    builder: (context) => GestureDetector(
+      onTap: () async {
+        if (await FlutterContacts.requestPermission()) {
+          var list = [primary];
+          await showCustomDialog(
+            context,
+            CustomDialog(
+              "Primary Contact",
+              ChooseContacts(
+                true,
+                await FlutterContacts.getContacts(),
+                list,
+              ),
+            ),
+          );
+          setState(() {
+            primary = list[0];
+          });
+        }
+      },
+      child: Container(
+        height: 75,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: white,
+            width: 4,
+          ),
+        ),
+        child: Center(
+          child: CustomText(
+            text: primary?.displayName ?? "Primary Contact",
+            color: white,
+            size: 20,
+          ),
+        ),
+      ),
+    ),
+  );
+
+  Widget secondaryContactsSelector() => GestureDetector(
+    onTap: () async {
+      if (await FlutterContacts.requestPermission()) {
+        await showCustomDialog(
+          context,
+          CustomDialog(
+            "Secondary Contacts",
+            ChooseContacts(
+              false,
+              await FlutterContacts.getContacts(),
+              secondaries,
+            ),
+          ),
+        );
+        setState(() {});
+      }
+    },
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: white,
+          width: 4,
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: secondaries.isNotEmpty ? 10 : 20),
+          child: CustomText(
+            text: secondaries.isEmpty ? "Secondary Contacts" : names(),
+            color: white,
+            size: 20,
+          ),
+        ),
+      ),
+    ),
+  );
+
+  String names() {
+    var string = "";
+    for (Contact c in secondaries) string += c.displayName + '\n';
+    return string.substring(0, string.length - 1);
+  }
+}
+
+class ChooseContacts extends StatefulWidget {
+
+  final bool chooseOne;
+  final List<Contact> contacts;
+  final List<Contact> selected;
+
+  ChooseContacts(this.chooseOne, this.contacts, this.selected);
+
+  @override
+  _ChooseContactsState createState() => _ChooseContactsState();
+}
+
+class _ChooseContactsState extends State<ChooseContacts> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 400,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: verticalSpace(10, widget.contacts.map<Widget>((contact) => GestureDetector(
+                onTap: () {
+                  setState(() {
+                    if (widget.selected.contains(contact)) {
+                      widget.selected.remove(contact);
+                    } else {
+                      if (widget.chooseOne) {
+                        widget.selected[0] = contact;
+                      } else {
+                        widget.selected.add(contact);
+                      }
+                    }
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: !widget.selected.contains(contact) ? white : background,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    child: CustomText(
+                      text: contact.displayName,
+                      size: 18,
+                      color: !widget.selected.contains(contact) ? background : white,
+                    ),
+                  ),
+                ),
+              )).toList()),
+            ),
+          ),
+        ),
+        SizedBox(height: 20,),
+        CustomButton(
+          text: "Done",
+          onPress: () => Navigator.of(context).pop(),
+        ),
+      ],
     );
   }
 }
---
+++ lib/widgets.dart
@@ -0,0 +1,177 @@
+import 'package:drunk_proj/constants.dart';
+import 'package:flutter/cupertino.dart';
+
+class CustomText extends StatelessWidget {
+
+  final String text;
+  final Color color;
+  final double size;
+
+  CustomText({this.text, this.color = white, this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+}
+
+class CustomButton extends StatefulWidget {
+
+  final double width;
+  final String text;
+  final VoidCallback onPress;
+
+  CustomButton({this.width, this.text, this.onPress,});
+
+  @override
+  _CustomButtonState createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+
+  bool isPressedDown = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerDown: (event) => setState(() => isPressedDown = true),
+      onPointerUp: (event) => setState(() => isPressedDown = false),
+      onPointerCancel: (event) => setState(() => isPressedDown = false),
+      child: GestureDetector(
+        onTap: widget.onPress,
+        child: AnimatedOpacity(
+          opacity: isPressedDown ? 0.6 : 1,
+          duration: Duration(milliseconds: isPressedDown ? 10 : 100),
+          child: Container(
+            height: 65,
+            width: widget.width,
+            decoration: BoxDecoration(
+              color: dark,
+              borderRadius: BorderRadius.circular(11),
+            ),
+            child: Center(
+              child: CustomText(
+                text: widget.text,
+                color: white,
+                size: 22,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+class CustomTextField extends StatefulWidget {
+
+  final TextEditingController controller;
+  final String placeholder;
+
+  CustomTextField({this.controller, this.placeholder});
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CupertinoTextField(
+          cursorColor: CupertinoTheme.of(context).primaryColor,
+          controller: widget.controller,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: white,
+              width: 4,
+            ),
+            borderRadius: BorderRadius.circular(11),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+          placeholder: widget.placeholder,
+          placeholderStyle: TextStyle(
+            color: white.withAlpha(220),
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+}
+
+class CustomDialog extends StatelessWidget {
+
+  final Widget child;
+  final String title;
+
+  CustomDialog(this.title, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      padding: MediaQuery.of(context).viewInsets,
+      duration: Duration(milliseconds: 200),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(),
+                          Align(
+                            alignment: Alignment.center,
+                            child: _title(context),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: child == null ? 0 : 15),
+                    child ?? Container(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _title(BuildContext context) {
+    return CustomText(
+      text: title,
+      color: dark,
+      size: 25,
+    );
+  }
+
+}
